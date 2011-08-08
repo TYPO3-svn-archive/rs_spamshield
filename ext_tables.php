29d16c72fab80dffda6ab99f5332d9cac77f8db9
acquire <acquire@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-08-08 21:56:44 +0000

    [~TWEAK] Removed wizard icon as it is useless because the plugin must be inserted only once ans also no title and description was shown
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/rs_spamshield/trunk@50779 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_tables.php
+++ ext_tables.php
@@ -12,9 +12,6 @@ t3lib_extMgm::addPlugin(array(
 	$_EXTKEY . '_pi1',
 	t3lib_extMgm::extRelPath($_EXTKEY) . 'ext_icon.gif'
 ),'list_type');
-if (TYPO3_MODE == 'BE') {
-	$TBE_MODULES_EXT['xMOD_db_new_content_el']['addElClasses']['tx_spamshield_pi1_wizicon'] = t3lib_extMgm::extPath($_EXTKEY).'pi1/class.tx_spamshield_pi1_wizicon.php';
-}
 #####################################################
 
 #####################################################
